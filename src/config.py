7c6df1aad7aeb8243f58dd5a5bf2860ecb776346
Dumitru <63454161+LopushanD@users.noreply.github.com>
2026-06-03 22:56:55 +0200

    containerize the project

--- src/config.py
+++ src/config.py
@@ -3,7 +3,9 @@ from pathlib import Path
 from typing import Final
 
 
-DEFAULT_DATABASE_PATH: Final[Path] = Path("gym_bot.sqlite3")
+DEFAULT_DATABASE_PATH: Final[Path] = Path(
+    os.environ.get("DATABASE_PATH", "gym_bot.sqlite3")
+)
 MAILBOX_MEMBER_ID: Final[int] = 1 #TODO REMOVE old code, used only in tests
 HANDOVER_WINDOW_SECONDS = 30
 BOT_ID: Final[str] = "t.me/api_test_4398523423043_bot"
